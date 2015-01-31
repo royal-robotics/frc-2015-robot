be65009b218bd482332872e3c05d218c60a6c8f5
blomqcon <connor.blomquists@gmail.com>
2015-01-31 14:46:40 -0800

    added ledcontroller class files

---
+++ src/org/usfirst/frc/team2522/robot/LEDOutput.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team2522.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class LEDOutput {
+	private Object output;
+	private boolean ledOn;
+	
+	public LEDOutput(DigitalOutput out) {
+		out.set(false);
+		output = out;
+	}
+	
+	public LEDOutput(PWM out) {
+		out.setRaw(0);
+		output = out;
+	}
+	
+	public LEDOutput(Solenoid out) {
+		out.set(true);
+		output = out;
+	}
+	
+	public void set(boolean on) {
+		ledOn = on;
+		if(output instanceof DigitalOutput) {
+			DigitalOutput led = (DigitalOutput) output;
+			led.set(ledOn);
+		} else if(output instanceof PWM) {
+			PWM led = (PWM) output;
+			if(ledOn) {
+				led.setRaw(255);
+			} else {
+				led.setRaw(0);
+			}
+		} else if(output instanceof Solenoid) {
+			Solenoid led = (Solenoid) output;
+			led.set(ledOn);
+		}
+	}
+	
+	public boolean get() {
+		return ledOn;
+	}
+}
