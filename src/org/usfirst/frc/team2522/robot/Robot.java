06c8ac196f44428d54b2276ac82de6814e622389
Programming Laptop <Robotics@10.85.32.62>
2015-02-21 10:42:17 -0800

    Added SmartDashboard, basic Autonomous, cleaned up code

--- src/org/usfirst/frc/team2522/robot/Robot.java
+++ src/org/usfirst/frc/team2522/robot/Robot.java
@@ -19,10 +19,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 
 	RobotDrive robotDrive;
-	
+
 	public static final double PULSES_PER_FOOT = 171.0;
 	public static final double ButtonDelay = 0.5;
-	//public static final int Swag = 1337;
+	// public static final int Swag = 1337;
 	public String CurrentAuto = " ";
 	// Joy sticks
 	// -------------------------------------------------------------------
@@ -51,13 +51,13 @@ public class Robot extends IterativeRobot {
 	DigitalInput liftLimitDown;
 	DigitalInput canLimitUp;
 	DigitalInput canLimitDown;
-	
+
 	// Encoders
 	// ---------------------------------------------------------------
 	Encoder rightDrive;
 	Encoder leftDrive;
 	Encoder lift;
-	
+
 	// LEDs
 	// ---------------------------------------------------------------
 	LEDController leds;
@@ -65,13 +65,13 @@ public class Robot extends IterativeRobot {
 	// ---------------------------------------------------------------
 	CameraServer server;
 
-	public void robotInit() {		
+	public void robotInit() {
 		// Init Joysticks
 		// ---------------------------------------------------------------
 		leftstick = new Joystick(0);
 		rightstick = new Joystick(1);
 		operatorstick = new Joystick(2);
-		
+
 		// Init Motors
 		// ---------------------------------------------------------------
 		frontRightChannel = new VictorSP(0);
@@ -84,32 +84,31 @@ public class Robot extends IterativeRobot {
 		canLeft = new Talon(7);
 		canHolder = new Relay(0);
 		holderTimer = new Timer();
-		
+
 		// Init Drive
 		// ---------------------------------------------------------------
 		robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel,
 				frontRightChannel, rearRightChannel);
 		robotDrive.setInvertedMotor(MotorType.kFrontRight, true);
 		robotDrive.setInvertedMotor(MotorType.kRearRight, true);
-		
+
 		// Init Limit Switches
 		// ---------------------------------------------------------------
 		liftLimitUp = new DigitalInput(0);
 		liftLimitDown = new DigitalInput(1);
 		canLimitUp = new DigitalInput(2);
 		canLimitDown = new DigitalInput(3);
-		
+
 		// Init Encoders
 		// ---------------------------------------------------------------
 		lift = new Encoder(new DigitalInput(5), new DigitalInput(4));
 		rightDrive = new Encoder(new DigitalInput(6), new DigitalInput(7));
 		leftDrive = new Encoder(new DigitalInput(9), new DigitalInput(8));
 
-		lift.setDistancePerPulse(1/7009);
-		rightDrive.setDistancePerPulse(1/PULSES_PER_FOOT);
-		leftDrive.setDistancePerPulse(1/PULSES_PER_FOOT);
-		
-		
+		lift.setDistancePerPulse(1 / 7009);
+		rightDrive.setDistancePerPulse(1 / PULSES_PER_FOOT);
+		leftDrive.setDistancePerPulse(1 / PULSES_PER_FOOT);
+
 		lift.reset();
 		rightDrive.reset();
 		leftDrive.reset();
@@ -122,165 +121,140 @@ public class Robot extends IterativeRobot {
 		// ---------------------------------------------------------------
 		server = CameraServer.getInstance();
 		server.startAutomaticCapture("cam0");
-		
+
 		// Init SmartDashboard
 		// ---------------------------------------------------------------
 		SmartDashboard.putNumber("Encoder Value: ", lift.get());
-		SmartDashboard.putNumber("Right Drive Encoder Value: ", rightDrive.get());
+		SmartDashboard.putNumber("Right Drive Encoder Value: ",
+				rightDrive.get());
 		SmartDashboard.putNumber("Left Drive Encoder Value: ", leftDrive.get());
 		SmartDashboard.putNumber("Can Lift Speed: ", 0.0);
 	}
-    
+
 	public void disabledPeriodic() {
 		leds.setColor(LEDUtil.Color.WHITE);
-        dashboardOutput(operatorstick.getY());
+		dashboardOutput(operatorstick.getY());
 	}
-	
+
 	int autoState = 0;
-	
-	public void autonomousInit()
-	{
+
+	public void autonomousInit() {
 		autoState = 0;
 
 		rightDrive.reset();
-		/*while(rightDrive.getDistance() < 9.0)
-		{
-			robotDrive.mecanumDrive_Cartesian(0, .5, 0, 0);
-			SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
-			SmartDashboard.putNumber("autonomousRight",rightDrive.getDistance());
-			
-		} 
-	    robotDrive.mecanumDrive_Cartesian(0,0,0,0);*/
-		/*while(liftLimitUp.get()) {
-			liftLeft.set(-0.75);
-			liftRight.set(0.75);
-		}
-		liftLeft.set(0);
-		liftRight.set(0);
-		while(rightDrive.getDistance() < -4.0)
-		{
-			robotDrive.mecanumDrive_Cartesian(0, .5, 0, 0);
-			SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
-			SmartDashboard.putNumber("autonomousRight",rightDrive.getDistance());
-			
-		} 
-	    robotDrive.mecanumDrive_Cartesian(0,0,0,0);*/
+		/*
+		 * while(rightDrive.getDistance() < 9.0) {
+		 * robotDrive.mecanumDrive_Cartesian(0, .5, 0, 0);
+		 * SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
+		 * SmartDashboard.putNumber("autonomousRight",rightDrive.getDistance());
+		 * 
+		 * } robotDrive.mecanumDrive_Cartesian(0,0,0,0);
+		 */
+		/*
+		 * while(liftLimitUp.get()) { liftLeft.set(-0.75); liftRight.set(0.75);
+		 * } liftLeft.set(0); liftRight.set(0); while(rightDrive.getDistance() <
+		 * -4.0) { robotDrive.mecanumDrive_Cartesian(0, .5, 0, 0);
+		 * SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
+		 * SmartDashboard.putNumber("autonomousRight",rightDrive.getDistance());
+		 * 
+		 * } robotDrive.mecanumDrive_Cartesian(0,0,0,0);
+		 */
 		liftUp = false;
-		SmartDashboard.putBoolean("LiftLimitHit",false);
+		SmartDashboard.putBoolean("LiftLimitHit", false);
 	}
-	
-	
-	
-	public void autonomousPeriodic() 
-	{
-		
-		SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
-		SmartDashboard.putNumber("autonomousRight",rightDrive.getDistance());
-		//Autonomus1
-		if (autoState == 0 && leftstick.getRawAxis(2) > 0)
-		{
-			if(rightDrive.getDistance() < 0.5)
-			{
-				robotDrive.mecanumDrive_Cartesian(0.0,-.3, 0.0, 0.0);
-				//SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
-				//SmartDashboard.putNumber("autnomousRight",rightDrive.getDistance());
-			}
-			else
-			{
-				robotDrive.mecanumDrive_Cartesian(0.0,0.0, 0.0, 0.0);
+
+	public void autonomousPeriodic() {
+
+		SmartDashboard.putNumber("autnomousLeft", leftDrive.getDistance());
+		SmartDashboard.putNumber("autonomousRight", rightDrive.getDistance());
+		// Autonomus1
+		if (autoState == 0 && leftstick.getRawAxis(2) > 0) {
+			if (rightDrive.getDistance() < 0.5) {
+				robotDrive.mecanumDrive_Cartesian(0.0, -.3, 0.0, 0.0);
+				// SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
+				// SmartDashboard.putNumber("autnomousRight",rightDrive.getDistance());
+			} else {
+				robotDrive.mecanumDrive_Cartesian(0.0, 0.0, 0.0, 0.0);
 				autoState++;
 			}
-		}
-		else if (autoState == 1 && leftstick.getRawAxis(2) > 0)
-		{
-			if(liftLimitUp.get()) 
-			{
-				if (!liftUp)
-				{
+		} else if (autoState == 1 && leftstick.getRawAxis(2) > 0) {
+			if (liftLimitUp.get()) {
+				if (!liftUp) {
 					liftLeft.set(-0.25);
 					liftRight.set(0.25);
 				}
-			}
-			else 
-			{
-				SmartDashboard.putBoolean("LiftLimitHit",true);
+			} else {
+				SmartDashboard.putBoolean("LiftLimitHit", true);
 				liftUp = true;
 				liftLeft.set(0.0);
 				liftRight.set(0.0);
 			}
-			
-			if(rightDrive.getDistance() > -8.0 && liftUp)
-			{
+
+			if (rightDrive.getDistance() > -8.0 && liftUp) {
 				robotDrive.mecanumDrive_Cartesian(0, .5, 0, 0);
-				//SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
-				//SmartDashboard.putNumber("autonomousRight",rightDrive.getDistance());
-				
+				// SmartDashboard.putNumber("autnomousLeft",leftDrive.getDistance());
+				// SmartDashboard.putNumber("autonomousRight",rightDrive.getDistance());
+
 			} else {
-			    robotDrive.mecanumDrive_Cartesian(0,0,0,0);
-			    if (liftUp)
-			    {
-				    autoState++;
-			    }
+				robotDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
+				if (liftUp) {
+					autoState++;
+				}
 			}
 		}
-		//Autonomus1 end
-		
-		//Autonomus2
-		if (autoState == 0 && leftstick.getRawAxis(2) < 0)
-		{
-			if (rightDrive.getDistance() < 20)
-			{
+		// Autonomus1 end
+
+		// Autonomus2
+		if (autoState == 0 && leftstick.getRawAxis(2) < 0) {
+			if (rightDrive.getDistance() < 20) {
 				robotDrive.mecanumDrive_Cartesian(.75, 0, 0, 0);
-			}  else {
+			} else {
 				robotDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
 				autoState++;
-				
+
 			}
 		}
 	}
-	
+
 	public void teleopPeriodic() {
 		// Set LED's
 		// ---------------------------------------------------------------
 		double drivePower = .5;
-		
+
 		if (leftstick.getRawButton(4)) {
 			leds.setColor(LEDUtil.Color.RED);
 		}
-		
+
 		if (leftstick.getRawButton(3)) {
 			leds.setColor(LEDUtil.Color.GREEN);
 		}
-		
+
 		if (leftstick.getRawButton(5)) {
 			leds.setColor(LEDUtil.Color.BLUE);
 		}
-		
+
 		if (leftstick.getRawButton(2)) {
 			leds.setColor(LEDUtil.Color.OFF);
 		}
-		
-		if(leftstick.getRawButton(1))
-		{
-			
+
+		if (leftstick.getRawButton(1)) {
+
 		}
-		
-		if(leftstick.getRawButton(1)||rightstick.getRawButton(1))
-		{
-			if(leftstick.getRawButton(1)&&rightstick.getRawButton(1))
-			{
-				drivePower=1;
+
+		if (leftstick.getRawButton(1) || rightstick.getRawButton(1)) {
+			if (leftstick.getRawButton(1) && rightstick.getRawButton(1)) {
+				drivePower = 1;
 			} else {
-				drivePower=.75;
+				drivePower = .75;
 			}
 		} else {
 			drivePower = .5;
 		}
-		//leds.setColor(LEDUtil.Color.GREEN);
+		// leds.setColor(LEDUtil.Color.GREEN);
 		// DeadZone Value
 		// ---------------------------------------------------------------
 		double deadzone = 0.15;
-		
+
 		// Drive Container Lift
 		// ---------------------------------------------------------------
 		if (operatorstick.getRawButton(4) && liftLimitUp.get()) {
@@ -293,34 +267,38 @@ public class Robot extends IterativeRobot {
 			liftLeft.set(0);
 			liftRight.set(0);
 		}
-		
-		if(!liftLimitDown.get())
-		{
+
+		if (!liftLimitDown.get()) {
 			lift.reset();
 		}
-		
-		
+
+		/*
+		 * double liftSpeed = operatorstick.getY(); if((canLift > 0) && (canLift
+		 * > deadzone)) { canLeft.set(-canLift*0.25);
+		 * canRight.set(canLift*0.25); }
+		 */
 		// Drive Can Lift
 		// ---------------------------------------------------------------
 		double canLift = operatorstick.getY();
-		if((canLift > 0) && (canLift > deadzone))
-		{
-				canLeft.set(-canLift*0.25);
-				canRight.set(canLift*0.25);
-		}
-		else if ((canLift < 0) && (canLift < -deadzone) && canLimitUp.get())
-		{
+		if ((canLift > 0) && (canLift > deadzone)) {
+			canLeft.set(-canLift * 0.25);
+			canRight.set(canLift * 0.25);
+		} else if ((canLift < 0) && (canLift < -deadzone) && canLimitUp.get()) {
+			if (operatorstick.getRawButton(6)) {
+				canLeft.set(-canLift / 2);
+				canRight.set(canLift / 2);
+			} else {
 				canLeft.set(-canLift);
 				canRight.set(canLift);
-		}
-		else
-		{
-				canLeft.set(0.0);
-				canRight.set(0.0);
+			}
+
+		} else {
+			canLeft.set(0.0);
+			canRight.set(0.0);
 		}
 
-		if(operatorstick.getRawButton(6) && resetButton) {
-			if(isIn){
+		if (operatorstick.getRawButton(5) && resetButton) {
+			if (isIn) {
 				canHolder.set(Value.kForward);
 				isIn = false;
 				Timer.delay(ButtonDelay);
@@ -333,82 +311,73 @@ public class Robot extends IterativeRobot {
 			canHolder.set(Value.kOff);
 			resetButton = true;
 		}
-		/*if (operatorstick.getRawButton(6)) {
-			canHolder.set(Value.kForward);
-		} else if(operatorstick.getRawButton(8)) {
-			canHolder.set(Value.kReverse);
-		} else {
-			canHolder.set(Value.kOff);
-		}*/
+		/*
+		 * if (operatorstick.getRawButton(6)) { canHolder.set(Value.kForward); }
+		 * else if(operatorstick.getRawButton(8)) {
+		 * canHolder.set(Value.kReverse); } else { canHolder.set(Value.kOff); }
+		 */
 
 		// Magnitude for Mecanum
 		// ---------------------------------------------------------------
-		/*double magnitude = rightstick.getMagnitude(); // Magnitude of Drive
-		if (magnitude < deadzone) // Dead Zone
-		{
-			magnitude = 0;
-		}
-		
-		if (leftstick.getTrigger() && rightstick.getTrigger())
-		{
-			SmartDashboard.putNumber("Drive Multiplier: ", 1);
-		}
-		else if (leftstick.getTrigger() || rightstick.getTrigger())
-		{
-			magnitude = magnitude * 0.75;
-			SmartDashboard.putNumber("Drive Multiplier: ", .75);
-		}
-		else
-		{
-			magnitude = magnitude * 0.50;
-			SmartDashboard.putNumber("Drive Multiplier: ", .5);
-		}
-		
-		SmartDashboard.putNumber("Drive Magnitude: ", magnitude);
-		*/
-		
+		/*
+		 * double magnitude = rightstick.getMagnitude(); // Magnitude of Drive
+		 * if (magnitude < deadzone) // Dead Zone { magnitude = 0; }
+		 * 
+		 * if (leftstick.getTrigger() && rightstick.getTrigger()) {
+		 * SmartDashboard.putNumber("Drive Multiplier: ", 1); } else if
+		 * (leftstick.getTrigger() || rightstick.getTrigger()) { magnitude =
+		 * magnitude * 0.75; SmartDashboard.putNumber("Drive Multiplier: ",
+		 * .75); } else { magnitude = magnitude * 0.50;
+		 * SmartDashboard.putNumber("Drive Multiplier: ", .5); }
+		 * 
+		 * SmartDashboard.putNumber("Drive Magnitude: ", magnitude);
+		 */
+
 		// X-Direction for Mecanum
 		// ---------------------------------------------------------------
 		double leftX = leftstick.getX();
 		if (leftX > -deadzone && leftX < deadzone) {
 			leftX = 0;
 		}
-		
+
 		// Y-Direction for Mecanum
 		// ---------------------------------------------------------------
 		double leftY = leftstick.getY();
 		if (leftY > -deadzone && leftY < deadzone) {
 			leftY = 0;
 		}
-		
+
 		// Rotation for Mecanum
 		// ---------------------------------------------------------------
 		double rotation = rightstick.getX();
 		if (rotation > -deadzone && rotation < deadzone) {
 			rotation = 0;
 		}
-		
+
 		// Mecanum Drive
 		// ---------------------------------------------------------------
-		robotDrive.mecanumDrive_Cartesian(leftX*drivePower, leftY*drivePower, rotation*drivePower, 0.0);
-		
+		robotDrive.mecanumDrive_Cartesian(leftX * drivePower, leftY
+				* drivePower, rotation * drivePower, 0.0);
+
 		// Encoder Test
 		dashboardOutput(canLift);
 	}
-	
+
 	public void dashboardOutput(double canLift) {
-		if(leftstick.getRawAxis(2) > 0);
+		if (leftstick.getRawAxis(2) > 0)
+			;
 		{
 			CurrentAuto = "Autonomus 1";
-		} 
-		if(leftstick.getRawAxis(2) < 0)
-		{
+		}
+		if (leftstick.getRawAxis(2) < 0) {
 			CurrentAuto = "Autonomus 2";
 		}
-		
+
 		SmartDashboard.putNumber("Lift Encoder Value: ", lift.get());
-		SmartDashboard.putNumber("Right Drive Encoder Value: ", rightDrive.getDistance());
-		SmartDashboard.putNumber("Left Drive Encoder Value: ", leftDrive.getDistance());
+		SmartDashboard.putNumber("Right Drive Encoder Value: ",
+				rightDrive.getDistance());
+		SmartDashboard.putNumber("Left Drive Encoder Value: ",
+				leftDrive.getDistance());
 		SmartDashboard.putNumber("Can Lift Speed: ", canLift);
 		SmartDashboard.putBoolean("LiftUp", liftLimitUp.get());
 		SmartDashboard.putBoolean("LiftDown", liftLimitDown.get());
@@ -416,7 +385,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putBoolean("CanDown", canLimitDown.get());
 		SmartDashboard.putNumber("Autonomous Mode", leftstick.getRawAxis(2));
 		SmartDashboard.putString("Current Autonomus", (String) CurrentAuto);
-		
-		//SmartDashboard.putNumber("ROYAL ROBOTICS SWAG AMOUNT", Swag);
+
+		// SmartDashboard.putNumber("ROYAL ROBOTICS SWAG AMOUNT", Swag);
 	}
 }
